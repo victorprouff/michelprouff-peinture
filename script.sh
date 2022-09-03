c2e8fcb464003b8ddccd0554b52fd4d4cabf0034
victorprouff <victorprouff@outlook.fr>
2022-09-03 16:16:19 +0200

    script

--- script.sh
+++ script.sh
@@ -1,5 +1,17 @@
 #!/usr/bin/env bash
 
+currentDate=`date +"%d-%m-%Y %T"`
+
 cd dest/michel-prouff-peinture-files/
-cp -R * ../../src
-sudo rm -R *
+cp -R ./dest/michel-prouff-peinture-files/* ./src
+sudo rm -R ./dest/michel-prouff-peinture-files/*
+
+# cd /home/victor/Documents/sites/michelprouff-peinture
+git add .
+git commit -m "Update Victor : ${currentDate}"
+git push
+
+cd /Users/victorprouff/Documents/Publii/sites
+git add .
+git commit -m "Update Victor : ${currentDate}"
+git push
